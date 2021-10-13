23f2957ee9841788b25b1ad5ed7b26cc4b964e8e
Viviana Diaz Velez <vdiazve@gmail.com>
2021-10-12 20:58:40 -0500

    spring initializr added

---
+++ sampleDevOps/src/main/java/co/edu/udem/sampleDevOps/SampleDevOpsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udem.sampleDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDevOpsApplication.class, args);
+	}
+
+}
